d10cc412c36c0122e19f8c85b966a19f439d8328
arjunsingh14 <arjunsingh140601@gmail.com>
2023-02-25 20:25:35 -0500

    Learnt parsing and variable assignment

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Calculator
+{
+    class Calculator
+    {
+        public static void Main () {
+            Console.Write("First Num: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Operator: ");
+            string op = Console.ReadLine();
+            Console.Write("Second Num: ");
+            double num2 = double.Parse(Console.ReadLine());
+            double output;
+
+            switch (op)
+            {
+                case "+":
+                    output = num1 + num2;
+                    break;
+                case "-":
+                    output = num1 - num2;
+                    break;
+                case "*":
+                    output = num1 * num2;
+                    break;
+                case "/":
+                    output = num1 / num2;
+                    break;
+                case "^":
+                    output = Math.Pow(num1, num2);
+                    break;
+                default:
+                    output = 0.0;
+                    break;
+            }
+            Console.WriteLine(output);
+
+        }
+
+
+    }
+}
+
---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace helloWorld{
+    class Program
+    {
+
+
+    public static void Main(string[] args)
+    {
+        string stringForFloat = "0.85"; // datatype should be float
+        string stringForInt = "12345"; // datatype should be int
+
+        float dec = float.Parse(stringForFloat);
+        int num = Int32.Parse(stringForInt);
+    }
+
+
+    }
+
+}
+
